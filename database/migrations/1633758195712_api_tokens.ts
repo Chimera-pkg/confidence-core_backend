0b8cbbc73dd9b5a85f9edd96cb38fa151dbaf76a
Chimera-pkg <adryanmagatsu88@gmail.com>
2025-04-25 15:32:32 +0700

    fix: adjust backend api_tokens data types

--- database/migrations/1633758195712_api_tokens.ts
+++ database/migrations/1633758195712_api_tokens.ts
@@ -6,17 +6,13 @@ export default class ApiTokens extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE') // Change to uuid
       table.string('name').notNullable()
       table.string('type').notNullable()
       table.string('token', 64).notNullable().unique()
 
       table.timestamp('expires_at', { useTz: true }).nullable()
 
-      /**
-       * Creates created_at and updated_at
-       * Set the default value to "CURRENT_TIMESTAMP"
-       */
       table.timestamps(true, true)
     })
   }
